6662aacd8f1f88025b2b009de50af7614e083301
gergokutu <gergo.kutu@gmail.com>
2019-07-09 14:45:49 +0200

    american grades 1

---
+++ americanGrades.js
@@ -0,0 +1,34 @@
+function toAmericanGrade(grade) {
+    // hint: you can use if ( *conditional* ) return 'A'; etc.
+    if (grade <= 10 && grade >= 9) {
+        return 'A'
+    } else if (grade <= 9 && grade >= 8) {
+        return 'B'
+    } else if (grade <= 8 && grade >= 7) {
+        return 'C'
+    } else if (grade <= 7 && grade >= 6) {
+        return 'D'
+    } else if (grade <= 6 && grade >= 1) {
+        return 'F'
+    } else {
+        return 'Not existing grade'
+    }
+  }
+  
+  // tests
+  function test (num, should_become) {
+    if (toAmericanGrade(num) === should_become) {
+      console.log("test passed")
+    } else {
+      console.log(`toAmericanGrade(${num}) should have been ${should_become} but was ${toAmericanGrade(num)}`)
+    }
+  }
+  test(8.7, "B")
+  test(10, "A")
+  test(1, "F")
+  test(5.9, "F")
+  test(6, "D")
+  test(6.9, "D")
+  test(7.3, "C")
+  test(0.3, "Not existing grade")
+  test(11, "Not existing grade")
